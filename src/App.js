1b5024b046ceea26e1a21ffc406fe59c0efaa8fd
Atanas Petrov <atanas.petrov@merkle.com>
2023-02-21 23:08:02 +0200

    mw-1 Moved html. Added homepage styles.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import BgVideo from "./components/BgVideo";
-import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div>
-      <BgVideo />
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/confirm" />
-        <Route path="/plan&locations" />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/confirm" />
+      <Route path="/plan&locations" />
+    </Routes>
   );
 }
 
