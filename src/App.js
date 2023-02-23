fd0389d6411d3ca3ff82481086319d0e99f3a4ab
mminchev94 <mlnminchevv@gmail.com>
2023-02-23 17:01:19 +0200

    Added 404 page and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage";
 import HomePage from "./pages/HomePage";
 import PlanPage from "./pages/PlanPage";
 
 function App() {
   return (
     <Routes>
+      <Route path="*" element={<NotFoundPage />} />
       <Route path="/" element={<HomePage />} />
       <Route path="/invitation" />
       <Route path="/plan&locations" element={<PlanPage />} />
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,10 @@
+import notFound from "../assets/notFound.png";
+function NotFoundPage() {
+  return (
+    <div class="notFound">
+      <img src={notFound} alt="Not Found Page" />
+    </div>
+  );
+}
+
+export default NotFoundPage;
