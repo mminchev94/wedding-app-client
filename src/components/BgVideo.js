98a6ea3f400b2012ebdcca454765dccf10a8d679
mminchev94 <mlnminchevv@gmail.com>
2023-03-13 20:05:22 +0200

    add playsInline for the video bg

--- src/components/BgVideo.js
+++ src/components/BgVideo.js
@@ -3,7 +3,7 @@ import video from "../assets/video.mp4";
 function BgVideo() {
   return (
     <div className="bgvideo">
-      <video src={video} autoPlay muted loop />
+      <video src={video} autoPlay muted loop playsInline />
     </div>
   );
 }
