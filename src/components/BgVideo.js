9f7854ca854398479b12e5e57ac78a6f3b34bbc6
mminchev94 <mlnminchevv@gmail.com>
2023-03-13 20:06:59 +0200

    Add disable pic in pic

--- src/components/BgVideo.js
+++ src/components/BgVideo.js
@@ -3,7 +3,14 @@ import video from "../assets/video.mp4";
 function BgVideo() {
   return (
     <div className="bgvideo">
-      <video src={video} autoPlay muted loop playsInline />
+      <video
+        src={video}
+        autoPlay
+        muted
+        loop
+        playsinline="true"
+        disablePictureInPicture="true"
+      />
     </div>
   );
 }
