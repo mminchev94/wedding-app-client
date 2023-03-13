f99bee322b32e9d94564610a8b157101350c254d
mminchev94 <mlnminchevv@gmail.com>
2023-03-13 23:05:01 +0200

    ----

--- src/components/BgVideo.js
+++ src/components/BgVideo.js
@@ -1,6 +1,28 @@
 import video from "../assets/video.mp4";
+import { useState, useEffect } from "react";
 
 function BgVideo() {
+  const [isLowPowerMode, setIsLowPowerMode] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+    setIsLowPowerMode(mediaQuery.matches);
+
+    const handleChange = (e) => {
+      setIsLowPowerMode(e.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleChange);
+    };
+  }, []);
+
+  if (isLowPowerMode) {
+    return null;
+  }
+
   return (
     <div className="bgvideo">
       <video type="video/mp4" src={video} autoPlay muted loop playsInline />
