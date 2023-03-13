c2ee372512dfa37766224ffdebe7354b9c12f0fc
mminchev94 <mlnminchevv@gmail.com>
2023-03-13 22:30:18 +0200

    Remove previous code

--- src/components/BgVideo.js
+++ src/components/BgVideo.js
@@ -1,41 +1,9 @@
-import { useState, useEffect } from "react";
 import video from "../assets/video.mp4";
 
 function BgVideo() {
-  const [isLowPowerMode, setIsLowPowerMode] = useState(false);
-
-  useEffect(() => {
-    const handleBatteryChange = () => {
-      setIsLowPowerMode(
-        window.navigator &&
-          window.navigator.battery &&
-          window.navigator.battery.level < 0.2
-      );
-    };
-
-    if (window.navigator && window.navigator.getBattery) {
-      navigator.getBattery().then((battery) => {
-        setIsLowPowerMode(battery.level < 0.2);
-        battery.addEventListener("levelchange", handleBatteryChange);
-      });
-    }
-
-    return () => {
-      if (window.navigator && window.navigator.getBattery) {
-        navigator.getBattery().then((battery) => {
-          battery.removeEventListener("levelchange", handleBatteryChange);
-        });
-      }
-    };
-  }, []);
-
   return (
     <div className="bgvideo">
-      {isLowPowerMode ? (
-        <div className="bgvideo-placeholder" />
-      ) : (
-        <video type="video/mp4" src={video} autoPlay muted loop playsInline />
-      )}
+      <video type="video/mp4" src={video} autoPlay muted loop playsInline />
     </div>
   );
 }
